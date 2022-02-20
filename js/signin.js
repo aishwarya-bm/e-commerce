4f8f4a9faa16869ac36e69145a9d07b1782b44a6
aishwarya-bm <biradaraishwarya@gmail.com>
2022-02-20 23:34:01 +0530

    changes for responsive card & other minor css
    1.card width changes for responsive
    2.removed repeated home button from home screen
    3.added redirection to signup on create new account link

--- js/signin.js
+++ js/signin.js
@@ -1,12 +1,12 @@
-const signupBtn = document.querySelector("#signup")
-const loginBtn = document.querySelector("#login")
+const signupBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const createAccountBtn = document.querySelector("#createAccount");
 
 const signupSection = document.querySelector("#signup-section");
 const loginSection = document.querySelector("#login-section");
 
 
 signupBtn.addEventListener("click", () => {
-
     loginSection.classList.add("hide");
     signupSection.classList.remove("hide");
     loginBtn.classList.remove("btn-active");
@@ -19,4 +19,11 @@ loginBtn.addEventListener("click", () => {
     loginSection.classList.remove("hide");
     loginBtn.classList.add("btn-active");
     signupBtn.classList.remove("btn-active");
+})
+
+createAccountBtn.addEventListener("click", () => {
+    loginSection.classList.add("hide");
+    signupSection.classList.remove("hide");
+    loginBtn.classList.remove("btn-active");
+    signupBtn.classList.add("btn-active");
 })
